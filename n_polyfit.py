26f661f3ea6af0cb88d12a86f84e4a81fe5af6ac
michaeljacot <michaeljacotguelph@gmail.com>
2021-01-08 15:45:35 -0400

    Got the program working. The script generates the polyfit polynomial for values 1 to n. Had to use the polyval function to convert the polyfit return vector with coeffs to actual readable data.

--- n_polyfit.py
+++ n_polyfit.py
@@ -3,5 +3,55 @@
 Created on Fri Jan  1 15:42:34 2021
 
 @author: Michael
+
+
+take in a dataset of closes, run polyfit on it with n = 1-10. See what it looks like
+
+edit: i forgot that polyfit doesnt return the actual plottable numbers, it returns the coeffs for the respective polynomial. Good thing numpy
+has a sick function polyval that does that for you.
+
+
 """
 
+
+import yfinance as yf
+import matplotlib.pyplot as plt
+import numpy as np
+
+def getData(stock,domain,window):
+    
+    tick = yf.Ticker(stock)
+    data = tick.history(period = domain,interval = window)
+    closes = data.Close.tolist()
+    
+    return closes,data
+
+
+
+stock = "BTC-USD"
+domain = "2y"
+window = "1d"
+
+closes,data = getData(stock,domain,window)
+time = np.arange(0,len(closes))
+
+
+plt.plot(closes)
+plt.title(stock + " over the last " + domain + " using closes every " + window)
+plt.show()
+
+
+
+
+#loop through each value for n and run polyfit with that value of n
+for i in range(1,25):
+    
+    thisPoly = np.polyfit(time,closes,i)
+    thisPoly = np.polyval(thisPoly,time)
+    
+    plt.plot(thisPoly)
+    plt.title("Polyfit where n =" + str(i))
+    plt.show()
+    
+    
+    
